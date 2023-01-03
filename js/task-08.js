13f1c2b5fbe88a580b41f0ee301fbc36db16ce93
Dmytro Potapenko <dima.lmkh@gmail.com>
2023-01-04 01:44:27 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", onSubmitFormSend);
 
+function onSubmitFormSend(event) {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+  //   console.log(formEl);
+  const { email, password } = formEl;
+  if (email.value.length < 1 || password.value < 1) {
+    return alert("Please enter the correct data");
+  }
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(userData);
+  form.reset();
+}
