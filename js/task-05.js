b457b3f1dc5a464831946f13b90cdc6844932a60
Dmytro Potapenko <dima.lmkh@gmail.com>
2023-01-05 18:15:40 +0200

    5 fix

--- js/task-05.js
+++ js/task-05.js
@@ -5,9 +5,16 @@ const output = document.querySelector("#name-output");
 input.addEventListener("input", onInputEnterText);
 
 function onInputEnterText(event) {
+  // console.log(event.currentTarget.value);
+  event.currentTarget.value = event.currentTarget.value.trimStart();
   output.textContent = event.currentTarget.value;
+  // console.log(output.textContent.trim());
+  // console.log(event.currentTarget.value.trim().length);
   // console.dir(event.currentTarget);
-  if (event.currentTarget.value.length === 0) {
+  if (
+    event.currentTarget.value.length === 0 ||
+    event.currentTarget.value.trim().length === 0
+  ) {
     output.textContent = "Anonymous";
   }
 }
