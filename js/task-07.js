f9b9b8740efd1b55fa5429b245fae62e95aeb2d8
Dmytro Potapenko <dima.lmkh@gmail.com>
2023-01-05 16:22:02 +0200

    7 fix

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,9 @@
 const input = document.querySelector("#font-size-control");
 const output = document.querySelector("#text");
+// input.value = 16;
+output.style.fontSize = `${input.value}px`;
 
 input.addEventListener("input", onInputTextSizeSettings);
-
 function onInputTextSizeSettings(event) {
   output.style.fontSize = `${event.currentTarget.value}px`;
 }
